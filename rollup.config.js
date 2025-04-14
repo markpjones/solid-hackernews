8c15eea00a999017375d8f144a38e1a1f9ff2623
markpjones <mark.p.jones@accenture.com>
2025-04-14 12:15:36 +1000

    Renamed to hex

--- rollup.config.js
+++ rollup.config.js
@@ -15,7 +15,7 @@ function template({ bundle }) {
   return `<!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Solid - Hacker News</title>
+    <title>Hex</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Hacker News Clone built with Solid">
